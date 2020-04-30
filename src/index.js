8544cd9034f8e0996e4c5f71a5827716f20a41e8
Sean Sundberg <seansund@us.ibm.com>
2020-04-30 07:11:52 -0500

    Adds lost handler for /

--- src/index.js
+++ src/index.js
@@ -8,4 +8,6 @@ app.get('/:name', (req, res) => {
     res.send(`Hello ${name}!`)
 })
 
+app.get('/', (req, res) => res.send(`Hello World!`))
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
