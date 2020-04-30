ef4203efc865287e169950e3b66d1c736a370986
Sean Sundberg <seansund@us.ibm.com>
2020-04-30 07:10:28 -0500

    Gets name from path

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/:name', (req, res) => {
+    const name = req.params.name
+
+    res.send(`Hello ${name}!`)
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
